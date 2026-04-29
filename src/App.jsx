2e18b456786625994b2bffa4a0f315dbbc114d68
out-of-mind-7 <abhijithsantosh234@gmail.com>
2026-04-29 13:01:24 +0530

    whatsapp number

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ export default function App() {
       <Services />
       <WhyChoose/>
       <Pricing />
-      <Transformations />
+      <Transformation />
       <Testimonials />
       <Contact />
 
       {/* WhatsApp Floating Button */}
       <a
-        href="https://wa.me/8111831107?text=Hi%20Nfit%20Club%2C%20I%20saw%20your%20website%20and%20I%27m%20interested%20in%20joining.%20Can%20you%20share%20pricing%20and%20timing%20details%3F"
+        href="https://wa.me/918111831107?text=Hi%20Nfit%20Club%2C%20I%20saw%20your%20website%20and%20I%27m%20interested%20in%20joining.%20Can%20you%20share%20pricing%20and%20timing%20details%3F"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:scale-110 transition z-50"
